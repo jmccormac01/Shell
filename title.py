d64fd606713d12f4986c9d8cdebdfcbc9122008a
jmccormac01 <jmccormac001@gmail.com>
2015-10-21 22:19:24 +0100

    update of title

--- title.py
+++ title.py
@@ -7,14 +7,11 @@ import os, sys
 
 def title (name):
   	os.system("printf '\033]0;%s\007'" % (name))
-	return 0
 	
 if __name__ == "__main__":
-	
 	if len(sys.argv) < 2:
 		print "usage: title name\n"
-		sys.exit()
-		
+		sys.exit()		
 	title(sys.argv[1])
 	
 	
